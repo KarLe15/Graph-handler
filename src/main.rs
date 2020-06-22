401597b31f0333558dff97901acf93f4f20e34f0
KarLe15 <leffadkarim97@live.fr>
2020-06-22 17:05:55 +0200

    Learning | implementing mandelbrot set in multithread | From Rust book | lookup crossbeam crate

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 
 extern crate num;
 extern crate image;
+extern crate crossbeam;
 
 use std::fs::File;
 use std::str::FromStr;
@@ -32,7 +33,8 @@ fn main() {
 //    println!("created node 3 {:?}", node3);
 //    let g = Graph::new(vec![1, 2, 3, 4], vec![(4,1), (1,3), (2,3), (1,4)]);
 //    println!("The graphe grated \n {:?}", g);
-    run_mandelbrot_single_thead();
+//    run_mandelbrot_single_thead();
+    run_mandelbrot_multi_theads();
 }
 
 fn run_mandelbrot_single_thead() {
@@ -57,6 +59,56 @@ fn run_mandelbrot_single_thead() {
         .expect("error parsing lower right corner point");
     let mut pixels = vec![0; bounds.0 * bounds.1];
     render(&mut pixels, bounds, upper_left, lower_right);
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
+}
+
+const LIMIT_MANDELBROT: u32 = 255;
+
+fn run_mandelbrot_multi_theads() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 5 {
+        writeln!(
+            std::io::stderr(),
+            "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT"
+        ).unwrap();
+        writeln!(
+            std::io::stderr(),
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        ).unwrap();
+        std::process::exit(1);
+    }
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+    // =================> the chnges start here
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+    {
+        let bands: Vec<&mut [u8]> =
+            pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left =
+                    pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height),
+                                   upper_left, lower_right);
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
+    // <====================== and ends here
     write_image(&args[1], &pixels, bounds)
         .expect("error writing PNG file");
 }
@@ -151,7 +203,7 @@ pub fn render(pixels: &mut [u8],
             let point = pixel_to_point(bounds, (column, row),
                                        upper_left, lower_right);
             pixels[row * bounds.0 + column] =
-                match escape_time(point, 255) {
+                match escape_time(point, LIMIT_MANDELBROT) {
                     None => 0,
                     Some(count) => 255 - count as u8
                 };
